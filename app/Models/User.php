e51c95df06bb697c831d1bc9894189b0bbd081d4
Phil Martinez <phil@philsquare.com>
2016-07-18 22:23:05 -0500

    seed users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Invoicing;
+namespace Invoicing\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,7 +11,7 @@
 |
 */
 
-$factory->define(Invoicing\User::class, function (Faker\Generator $faker) {
+$factory->define(Invoicing\Models\User::class, function (Faker\Generator $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->safeEmail,
