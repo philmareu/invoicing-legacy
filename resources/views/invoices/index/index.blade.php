ae8d36d289c02fa9901185067e3ff311cdd2f884
Phil Martinez <phil@philsquare.com>
2016-07-21 19:12:40 -0500

    small style update to invoices

--- resources/views/invoices/index/index.blade.php
+++ resources/views/invoices/index/index.blade.php
@@ -4,6 +4,14 @@
 
     <h1><i class="uk-icon-money"></i> Invoices</h1>
 
-    @include('invoices.index.table')
+    <div class="uk-panel uk-panel-box">
+        <h3 class="uk-panel-title"><i class="uk-icon-info"></i> Invoices</h3>
+
+        <div class="uk-grid">
+            <div class="uk-width-1-1">
+                @include('invoices.index.table')
+            </div>
+        </div>
+    </div>
 
 @endsection
