0452e797378ac7d3cd9307f7e500b5a42cbdcd73
Phil Martinez <phil@philsquare.com>
2016-07-21 19:23:02 -0500

    style update to invoice show page

--- resources/views/invoices/show/index.blade.php
+++ resources/views/invoices/show/index.blade.php
@@ -7,63 +7,85 @@
 
 @section('content')
 
-    <h1><i class="uk-icon-money"></i> Invoice {{ $invoice->invoice_number }}</h1>
-    <a href="{{ url(route('invoice.view', [$invoice->client_id, $invoice->unique_id])) }}">View</a>
-    <div class="info">
-        {{ $invoice->client->title }}
+
+    <div class="uk-panel uk-panel-box uk-margin-bottom">
+        <h3 class="uk-panel-title"><i class="uk-icon-info"></i> Invoice {{ $invoice->invoice_number }}</h3>
+        <div class="uk-panel-badge"><a href="{{ url(route('invoice.view', [$invoice->client_id, $invoice->unique_id])) }}">View</a></div>
+
+        <div class="uk-grid">
+            <div class="uk-width-1-1">
+                {{ $invoice->client->title }}
+            </div>
+        </div>
     </div>
 
-    <div class="items">
-        <h2>Items</h2>
-        <table class="uk-table uk-table-condensed">
-            <thead>
-                <tr>
-                    <th>Item</th>
-                    <th class="uk-text-right">Amount</th>
-                </tr>
-            </thead>
-            <tbody>
-                @each('invoices.show.item', $invoice->items, 'item')
-            </tbody>
-        </table>
-        <a href="{{ route('invoices.invoice-items.create', ['invoice_id' => $invoice->id]) }}" class="uk-button">Add</a>
+    <div class="uk-panel uk-panel-box uk-margin-bottom">
+        <h3 class="uk-panel-title"><i class="uk-icon-info"></i> Items</h3>
+        <div class="uk-panel-badge"><a href="{{ route('invoices.invoice-items.create', ['invoice_id' => $invoice->id]) }}">Add</a></div>
+
+        <div class="uk-grid">
+            <div class="uk-width-1-1">
+                <h2>Items</h2>
+                <table class="uk-table uk-table-condensed">
+                    <thead>
+                    <tr>
+                        <th>Item</th>
+                        <th class="uk-text-right">Amount</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @each('invoices.show.item', $invoice->items, 'item')
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
-    <div class="work-orders">
-        <h2>Work Orders</h2>
-        <table class="uk-table uk-table-condensed">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Status</th>
-                    <th>Time</th>
-                    <th>Rate</th>
-                    <th class="uk-text-right">Amount</th>
-                </tr>
-            </thead>
-            <tbody>
-                @each('invoices.show.workorder', $invoice->workOrders, 'workOrder')
-            </tbody>
-        </table>
-        <a href="{{ route('invoices.work-orders.create', ['invoice_id' => $invoice->id]) }}" class="uk-button">Add</a>
+    <div class="uk-panel uk-panel-box uk-margin-bottom">
+        <h3 class="uk-panel-title"><i class="uk-icon-info"></i> Work Orders</h3>
+        <div class="uk-panel-badge"><a href="{{ route('invoices.work-orders.create', ['invoice_id' => $invoice->id]) }}">Add</a></div>
+
+        <div class="uk-grid">
+            <div class="uk-width-1-1">
+                <table class="uk-table uk-table-condensed">
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Status</th>
+                        <th>Time</th>
+                        <th>Rate</th>
+                        <th class="uk-text-right">Amount</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @each('invoices.show.workorder', $invoice->workOrders, 'workOrder')
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
-    <div class="payments">
-        <h2>Payments</h2>
-        <table class="uk-table uk-table-condensed">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Payment Type</th>
-                    <th>Note</th>
-                    <th class="uk-text-right">Amount</th>
-                </tr>
-            </thead>
-            <tbody>
-                @each('invoices.show.payment', $invoice->payments, 'payment')
-            </tbody>
-        </table>
-        <a href="{{ route('invoices.payments.create', ['invoice_id' => $invoice->id]) }}" class="uk-button">Add</a>
+    <div class="uk-panel uk-panel-box uk-margin-bottom">
+        <h3 class="uk-panel-title"><i class="uk-icon-info"></i> Payments</h3>
+        <div class="uk-panel-badge"><a href="{{ route('invoices.payments.create', ['invoice_id' => $invoice->id]) }}">Add</a></div>
+
+        <div class="uk-grid">
+            <div class="uk-width-1-1">
+                <table class="uk-table uk-table-condensed">
+                    <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Payment Type</th>
+                        <th>Note</th>
+                        <th class="uk-text-right">Amount</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @each('invoices.show.payment', $invoice->payments, 'payment')
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
     <h2>Total</h2>
