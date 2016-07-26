4af10b948f1d57229a6d86234d297bb05b7f9121
Phil Martinez <phil@philsquare.com>
2016-07-26 10:00:12 -0500

    cleaned up view

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -7,25 +7,23 @@
 
 				<div class="uk-dropdown uk-dropdown-search"></div>
 			</form>
-			
+
 		</div>
 	</div>
 
 	<div class="uk-width-1-2">
 		<ul class="uk-subnav uk-subnav-line uk-float-right">
-
-
                     @if(! is_null($timer))
                         <span class="timer" data-invoicing-work-order-id="{{ $timer->work_order_id }}">
                             <a href="{{ route('work-orders.show', $timer->work_order_id) }}">{{ $timer->elapsedFormatted() }}</a>
                         </span>
                     @else
-                        <span class="timer" data-invoicing-work-order-id="">
+                        <span class="timer" data-invoicing-work-order-id="{{ $time->work_order_id }}">
                         </span>
                     @endif
 
                 <li data-uk-dropdown="{mode:'click'}">
-`
+
 				<a href="#">{{ Auth::user()->name }} <i class="uk-icon-toggle-down"></i></a>
 
 				<div class="uk-dropdown uk-dropdown-small">
