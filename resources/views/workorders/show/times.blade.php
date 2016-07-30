a60261c212b0f270aa3aeab3dfbf5efc9c0b6f57
Phil Martinez <phil@philsquare.com>
2016-07-30 10:58:51 -0500

    fixed missing timer on new work order

--- resources/views/workorders/show/times.blade.php
+++ resources/views/workorders/show/times.blade.php
@@ -4,42 +4,36 @@
 <div class="uk-grid">
 	<div class="uk-width-1-1 timesheet">
 
-@if(count($workOrder->times))
+        <div class="uk-overflow-container">
 
-<div class="uk-overflow-container">
+            @if(! is_null($timer) && $timer->work_order_id == $workOrder->id)
+                <button type="button" class="uk-button toggle-time uk-text-danger uk-width-1-1" id="{{ $workOrder->id }}">
+                    <i class="uk-icon-stop"></i> <i class="uk-icon-clock-o uk-icon-spin"></i> <span class="timer">{{ $timer->elapsedFormatted() }}</span>
+                </button>
+            @else
+                <button type="button" class="uk-button toggle-time uk-text-success uk-width-1-1" id="{{ $workOrder->id }}">
+                    <i class="uk-icon-play"></i> Start Timer
+                </button>
+            @endif
 
-    @if(! is_null($timer) && $timer->work_order_id == $workOrder->id)
-        <button type="button" class="uk-button toggle-time uk-text-danger uk-width-1-1" id="{{ $workOrder->id }}">
-            <i class="uk-icon-stop"></i> <i class="uk-icon-clock-o uk-icon-spin"></i> <span class="timer">{{ $timer->elapsedFormatted() }}</span>
-        </button>
-    @else
-        <button type="button" class="uk-button toggle-time uk-text-success uk-width-1-1" id="{{ $workOrder->id }}">
-            <i class="uk-icon-play"></i> Start Timer
-        </button>
-    @endif
+            <table class="uk-table uk-table-striped uk-table-condensed times uk-text-nowrap">
+                <thead>
+                <tr>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>&nbsp;</th>
+                </tr>
+                </thead>
 
-<table class="uk-table uk-table-striped uk-table-condensed times uk-text-nowrap">
-	<thead>
-	<tr>
-		<th>Date</th>
-		<th>Time</th>
-		<th>&nbsp;</th>
-	</tr>
-	</thead>
-	
-	<tbody>
-	@foreach($workOrder->times as $time)
-        @if($time->time)
-		    @include('times.partials.row')
-        @endif
-	@endforeach
-	</tbody>
-</table>
-</div>
-
-@else
-	<p class="uk-text-muted no-times">There are no times logged for this work order.</p>
-@endif
+                <tbody>
+                @foreach($workOrder->times as $time)
+                    @if($time->time)
+                        @include('times.partials.row')
+                    @endif
+                @endforeach
+                </tbody>
+            </table>
+        </div>
 
 </div>
 </div>
