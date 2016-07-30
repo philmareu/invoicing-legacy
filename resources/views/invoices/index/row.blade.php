6f3dd81bf021c44314dd12b106304ca2ac7570d1
Phil Martinez <phil@philsquare.com>
2016-07-30 10:05:43 -0500

    added links to clients

--- resources/views/invoices/index/row.blade.php
+++ resources/views/invoices/index/row.blade.php
@@ -1,6 +1,6 @@
 <tr>
     <td><a href="{{ route('invoices.show', $invoice->id) }}">{{ $invoice->invoice_number }}</a></td>
-    <td>{{ $invoice->client->title }}</td>
+    <td><a href="{{ route('clients.show', $invoice->client->id) }}">{{ $invoice->client->title }}</a></td>
     <td>{{ $invoice->due->format('M d, Y') }}</td>
     <td>${{ number_format($invoice->balance / 100, 2) }}</td>
 </tr>
--- resources/views/workorders/index/row.blade.php
+++ resources/views/workorders/index/row.blade.php
@@ -1,6 +1,6 @@
 <tr>
     <td><a href="{{ route('work-orders.show', $workOrder->id) }}">{{ $workOrder->id }}</a></td>
-    <td>{{ $workOrder->client->title }}</td>
+    <td><a href="{{ route('clients.show', $workOrder->client->id) }}">{{ $workOrder->client->title }}</a></td>
     <td>{{ $workOrder->reference }}</td>
     @if(is_null($workOrder->scheduled))
         <td>Not Scheduled</td>
